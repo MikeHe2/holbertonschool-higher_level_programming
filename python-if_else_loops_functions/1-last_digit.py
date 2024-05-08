7828302773d08ceaa543939fafee90533cee1b31
MikeHe2 <michael.he30@hotmail.com>
2024-05-08 15:18:02 -0400

    Fix typo in 1-last-digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ else:
 if LastNum > 5:
     print(f"Last digit of {number} is {LastNum} and is greater than 5")
 elif LastNum == 0:
-    print(f"last digit of {number} is {LastNum} and is 0")
+    print(f"Last digit of {number} is {LastNum} and is 0")
 elif LastNum < 6 and LastNum != 0:
     print(f"Last digit of {number} is {LastNum} and is less than 6 and not 0")
