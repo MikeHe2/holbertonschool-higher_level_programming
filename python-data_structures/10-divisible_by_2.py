7a9e1c50b038c9eed26c7416f56e2c8f998dbcc0
MikeHe2 <michael.he30@hotmail.com>
2024-05-13 21:47:09 -0400

    Add 10-divisible_by_2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    newStr = []
+
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            newStr.append(True)
+        else:
+            newStr.append(False)
+
+    return newStr
