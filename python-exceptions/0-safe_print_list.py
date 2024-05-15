5a7fb445a27a0d1f9512b18b4b6a5c2183482a65
MikeHe2 <michael.he30@hotmail.com>
2024-05-15 11:16:13 -0400

    Fix pycodstyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
 
         numIterates = 0
 
-        for i in range (x):
+        for i in range(x):
             print("{:d}".format(my_list[i]), end='')
             numIterates += 1
         print()
