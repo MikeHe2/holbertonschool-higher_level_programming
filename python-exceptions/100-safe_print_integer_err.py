fe5774f3082b8fdf1e86bb80987f681c5cfa45c8
MikeHe2 <michael.he30@hotmail.com>
2024-05-15 16:26:32 -0400

    Fix typo

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -8,7 +8,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
 
-    except(ValueError, TypeError):
+    except (ValueError, TypeError):
         sys.stderr.write("Exception: Unknown format code 'd' \
 for object of type 'str'\n")
         return False
