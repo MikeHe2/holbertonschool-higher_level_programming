3c844b1afbf6b7a8079fcd4b05237d19efcf83ff
MikeHe2 <michael.he30@hotmail.com>
2024-05-15 16:49:20 -0400

    Add final fix

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-def safe_print_integer_err(value):
+import sys
 
-    import sys
+
+def safe_print_integer_err(value):
 
     try:
         print("{:d}".format(value))
         return True
 
-    except (ValueError, TypeError) as x:
-        print("Expectaion: {}".format(x), file=sys.stderr)
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    try:
+        for i in range(list_lenght):
+            try:
+                
+
+
+    finally:
