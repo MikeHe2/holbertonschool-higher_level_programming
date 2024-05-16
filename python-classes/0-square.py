9f7ab9822a8bf55a42177d71b1e37ee4eddcb390
MikeHe2 <michael.he30@hotmail.com>
2024-05-16 14:54:49 -0400

    First commit: Add README.md, 0-square

---
+++ python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+my_module
+---------
+
+Este módulo contiene la clase `Square`, la cual actualmente no tiene
+ninguna funcionalidad implementada.
+
+"""
+
+class Square:
+    """
+    Square es una clase vacía que representa un cuadrado.
+
+    Actualmente no tiene atributos ni métodos.
+    """
+    pass
