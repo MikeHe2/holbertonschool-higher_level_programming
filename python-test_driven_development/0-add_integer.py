536b8a1f5d9ec13a88b72c20fd894b0521520fb7
MikeHe2 <michael.he30@hotmail.com>
2024-05-20 13:23:40 -0400

    Fix 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,11 @@
 
 def add_integer(a, b=98):
 
+    """
+
+    This is a documentation.
+
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
