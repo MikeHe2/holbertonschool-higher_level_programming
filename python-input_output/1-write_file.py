3e45a4c3f717faa47b767afe5ea232deae1754db
MikeHe2 <michael.he30@hotmail.com>
2024-05-28 10:34:16 -0400

    Fix documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
+"""This a module"""
 
+
+def write_file(filename="", text=""):
+    """
+    Writes the given text to a file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        Defaults to an empty string.
+        text (str): The text to write to the file. Defaults to an empty string.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
