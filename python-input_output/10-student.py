8f47eeab71f9c4a33653f3250ec0cea362009929
MikeHe2 <michael.he30@hotmail.com>
2024-05-28 23:13:18 -0400

    ADD: add 10-students

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""This is a module"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list): List of attributes to include in the dictionary
+            representation. If None, all attributes are included.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            if isinstance(attrs, list) and all(isinstance(attr, str)\
+                for attr in attrs):
+
+                return {attr: self.__dict__.get(attr) for attr in attrs\
+                    if attr in self.__dict__}
+            else:
+                return self.__dict__
