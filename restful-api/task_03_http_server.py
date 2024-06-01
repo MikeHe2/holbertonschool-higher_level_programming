1597bff0ff71f4ac030eda378d96b0265bf32b97
MikeHe2 <michael.he30@hotmail.com>
2024-06-01 15:39:51 -0400

    ADD: fix in task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(b"OK")
 
