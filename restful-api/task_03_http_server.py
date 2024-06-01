6f8ad305f7a07648bd3e8d245363464e396c2473
MikeHe2 <michael.he30@hotmail.com>
2024-06-01 15:22:00 -0400

    ADD: add task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,7 +30,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 not found")
 
 Handler = SimpleHTTPRequestHandler
 
