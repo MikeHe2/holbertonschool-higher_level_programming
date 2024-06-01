fe1102bbc3cf150aa2d3f4f8f4a248cd66cf63df
MikeHe2 <michael.he30@hotmail.com>
2024-06-01 15:38:33 -0400

    ADD: fix in task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,26 +15,30 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+
         elif self.path == '/data':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             response = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(response).encode())
+
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode())
+
         elif self.path == '/status':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(b"404 not found")
 
