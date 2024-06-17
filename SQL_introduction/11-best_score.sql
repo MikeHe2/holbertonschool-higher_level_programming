8a40c81dbf7ddfa9932f925cf5ce06580cedd3ac
MikeHe2 <michael.he30@hotmail.com>
2024-06-17 16:01:27 -0400

    ADD: add Task 11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
 -- Script that lists all records with a score >= 10 of the table second_table of a databas
-SELECT score, name FROM second_table WHERE socre >= 10;
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
