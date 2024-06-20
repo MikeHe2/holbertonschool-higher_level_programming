9234a782a1e53339a1dd0e7defe615b46ce628e3
MikeHe2 <michael.he30@hotmail.com>
2024-06-20 13:48:55 -0400

    ADD: add Task 0 and README

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and selects all rows from the 'states' table.
+The selected rows are then printed to the console.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
