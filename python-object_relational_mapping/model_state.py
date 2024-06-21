3fc47ae824ff0ed051d21070e5b29925420e36d7
MikeHe2 <michael.he30@hotmail.com>
2024-06-21 11:47:35 -0400

    Refactor model_state.py for improved readability and maintainability

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -19,5 +19,6 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
     name =  Column(String(128), nullable=False)
