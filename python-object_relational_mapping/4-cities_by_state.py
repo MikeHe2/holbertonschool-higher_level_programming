14c9ef2ffba8d04170f41d93db1cd409d598229b
MikeHe2 <michael.he30@hotmail.com>
2024-06-20 21:42:34 -0400

    ADD: add Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves data
+from two tables: cities and states. It then prints the id,
+name of the city, and name of the state for each row in the result set.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                INNER JOIN states ON states.id = state_id
+                ORDER BY cities.id ASC """)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
