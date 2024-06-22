22590f5747f8dea2227937fc048e47b91aeb7981
MikeHe2 <michael.he30@hotmail.com>
2024-06-22 00:01:11 -0400

    ADD: add model_city

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This module defines the State class, which represents a state in a database.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """
+    This class represents a state in a database.
+
+    Attributes:
+        id (int): The unique identifier of the state.
+        name (str): The name of the state.
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(ForeignKey("states.id"), Integer, nullable=False)
