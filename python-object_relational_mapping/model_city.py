1e6c6740f3d95c523b865944a69228e89b00b810
MikeHe2 <michael.he30@hotmail.com>
2024-06-22 00:15:31 -0400

    aawd

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,17 +8,17 @@ from model_state import Base, State
 
 
 class City(Base):
-	"""
-	City class represents a city in the database.
+    """
+    City class represents a city in the database.
 
-	Attributes:
-		id (int): The unique identifier for the city.
-		name (str): The name of the city.
-		state_id (int): The foreign key referencing the associated state.
+    Attributes:
+        id (int): The unique identifier for the city.
+        name (str): The name of the city.
+        state_id (int): The foreign key referencing the associated state.
 
-	"""
-	__tablename__ = 'cities'
-	id = Column(Integer, unique=True, primary_key=True,
-				autoincrement=True, nullable=False)
-	name = Column(String(128), nullable=False)
-	state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
