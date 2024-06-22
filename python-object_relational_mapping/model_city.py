3e0f792704514b8b617afb30b1ef59d3e1db724d
MikeHe2 <michael.he30@hotmail.com>
2024-06-22 00:03:05 -0400

    FIX??

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,7 +5,6 @@ This module defines the State class, which represents a state in a database.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 
