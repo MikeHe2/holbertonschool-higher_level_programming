0e5d390f07e9590255ff773c96bad20fe1b37667
MikeHe2 <michael.he30@hotmail.com>
2024-07-26 15:25:37 -0400

    feat: add event listener to toggle header color between red and green

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,9 @@
+// toggle = document.getElementById('toggle_header').style.color
+const button = document.getElementById("toggle_header");
+button.addEventListener("click", function() {
+    if (document.querySelector('header').classList.contains('green')) {
+        document.querySelector('header').classList.replace('green', 'red');
+    } else {
+        document.querySelector('header').classList.replace('red', 'green');
+    }
+});
